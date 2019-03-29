7a8350a489c89f18668c90b9b007279cc29f6ce0
wangruigen <1028725142@qq.com>
2019-03-29 18:07:30 +0800

    springcloud-study

---
+++ eureka-service-descovery/src/main/java/com/study/springcloud/EurekaServiceDescoveryApplication.java
@@ -0,0 +1,15 @@
+package com.study.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServiceDescoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceDescoveryApplication.class, args);
+    }
+
+}
---
+++ eureka-service-descovery/src/test/java/com/study/springcloud/EurekaServiceDescoveryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.study.springcloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaServiceDescoveryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
