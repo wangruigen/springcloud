00c49edd29bedd156c357c45be0791557c942823
wangruigen <1028725142@qq.com>
2019-04-04 13:45:36 +0800

    配置自定义feign client

--- movie-comsumer-feign-customize/src/main/java/com/study/springcloud/feignclient/UserFeignClient.java
+++ movie-comsumer-feign-customize/src/main/java/com/study/springcloud/feignclient/UserFeignClient.java
@@ -11,7 +11,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
  * 先创建一个UserFeignClient接口
  */
 /*
-配置注解FeignClient，指定eureka server列表中的服务，这里是用户提供者服务实例名.path表示的是应用名，可理解成tomcat root path
+配置注解FeignClient，指定eureka server列表中的服务，这里是用户提供者服务实例名.path(Path prefix)表示的是应用名，可理解成tomcat root path
    里面还有很多参数，简单讲url,url指定服务端地址，这里配合eureka使用，自动从eureka获取服务，可以不写。
 configuration:配置自己自定义的feign client
  */
