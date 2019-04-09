3b3a03d51da7a2798506cfb6ddb80f017c30cce7
wangruigen <1028725142@qq.com>
2019-04-09 22:43:02 +0800

    hystrix可视化监控

--- movie-comsumer-feign-hystrix-monitor/src/main/java/com/study/springcloud/MovieComsumerApplication.java
+++ movie-comsumer-feign-hystrix-monitor/src/main/java/com/study/springcloud/MovieComsumerApplication.java
@@ -3,11 +3,11 @@ package com.study.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-@EnableFeignClients //开启feignClient
 @EnableHystrixDashboard //开启hystrix可视化监控
 public class MovieComsumerApplication {
 
--- movie-comsumer-feign-hystrix-monitor/src/main/java/com/study/springcloud/controller/MovieController.java
+++ movie-comsumer-feign-hystrix-monitor/src/main/java/com/study/springcloud/controller/MovieController.java
@@ -1,27 +1,16 @@
 package com.study.springcloud.controller;
 
 import com.study.springcloud.entity.User;
-import com.study.springcloud.feignclient.UserFeignClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MovieController {
-    /**
-     * MovieController 通过注入UserFeignCLient的方式调用http api
-     */
-    private final UserFeignClient userFeignClient;
-
-    @Autowired
-    public MovieController(UserFeignClient userFeignClient) {
-        this.userFeignClient = userFeignClient;
-    }
 
     @GetMapping("/user/{id}")
     public User getUserById(@PathVariable String id) {
 
-        return userFeignClient.findById(id);
+        return null;
     }
 }
