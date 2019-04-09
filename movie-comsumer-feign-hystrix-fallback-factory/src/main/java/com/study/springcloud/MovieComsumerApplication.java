70e7bcb1eef306d4d402615d31c17e76281c89bf
wangruigen <1028725142@qq.com>
2019-04-09 22:10:27 +0800

    指定name名称的feignClient ，禁用hystrix配置，或者在配置文件配置feign.hystrix.enabled=false

--- movie-comsumer-feign-hystrix-fallback-factory/src/main/java/com/study/springcloud/MovieComsumerApplication.java
+++ movie-comsumer-feign-hystrix-fallback-factory/src/main/java/com/study/springcloud/MovieComsumerApplication.java
@@ -2,10 +2,12 @@ package com.study.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients //开启feignClient
+@EnableCircuitBreaker //开启hystrix监控
 public class MovieComsumerApplication {
 
     public static void main(String[] args) {
