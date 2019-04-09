5fecdb5bb79ae45966262636c4ad9407317425ac
wangruigen <1028725142@qq.com>
2019-04-09 21:39:15 +0800

    指定name名称的feignClient ，禁用hystrix配置，或者在配置文件配置feign.hystrix.enabled=false

--- movie-comsumer-feign-hystrix-fallback-factory/src/main/java/com/study/springcloud/feignclient/UserFeignClient.java
+++ movie-comsumer-feign-hystrix-fallback-factory/src/main/java/com/study/springcloud/feignclient/UserFeignClient.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 配置注解FeignClient，指定eureka server列表中的服务，这里是用户提供者服务实例名.path表示的是应用名，可理解成tomcat root path
    里面还有很多参数，简单讲url,url指定服务端地址，这里配合eureka使用，自动从eureka获取服务，可以不写。
  */
-// 指定name名称的feignClient ，禁用hystrix配置，或者在配置文件配置feign.hystrix.enabled=false
+// 指定name名称的feignClient ，禁用hystrix配置，或者在配置文件配置feign.hystrix.enabled=false(全局禁用)
 // @FeignClient(name = "user-provider",configuration = FeignDisableHystrixConfiguration.class)
 @FeignClient(name = "user-provider",path = "/user-provider",fallbackFactory = FeignClientFallBackFactory.class)//使用fallbackFactory
 public interface UserFeignClient {
