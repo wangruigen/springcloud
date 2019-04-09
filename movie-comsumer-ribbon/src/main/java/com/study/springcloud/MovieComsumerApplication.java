96c3582f5b84691f89723498a081004d51d09885
wangruigen <1028725142@qq.com>
2019-04-09 19:27:06 +0800

    电影微服务-服务消费者-整合ribbon、hystrix

--- movie-comsumer-ribbon/src/main/java/com/study/springcloud/MovieComsumerApplication.java
+++ movie-comsumer-ribbon/src/main/java/com/study/springcloud/MovieComsumerApplication.java
@@ -2,8 +2,12 @@ package com.study.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+//@EnableHystrix //开启断路器支持,或者下面的注解也可以
+@EnableCircuitBreaker
 public class MovieComsumerApplication {
 
     public static void main(String[] args) {
--- movie-comsumer-ribbon/src/main/java/com/study/springcloud/controller/MovieController.java
+++ movie-comsumer-ribbon/src/main/java/com/study/springcloud/controller/MovieController.java
@@ -1,5 +1,6 @@
 package com.study.springcloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.study.springcloud.entity.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,17 +28,50 @@ public class MovieController {
         this.loadBalancerClient = loadBalancerClient;
     }
 
+    @HystrixCommand(fallbackMethod = "getUserByIdFallBack")
     @GetMapping("/user/{id}")
-    public User getUserById(@PathVariable String id){
-        ResponseEntity<User> response = restTemplate.getForEntity("http://localhost:8088/user-provider/user/"+id, User.class);
+    public User getUserById(@PathVariable String id) {
+        String url = "http://user-provider/user-provider/user/" + id;
+        ResponseEntity<User> response = restTemplate.getForEntity(url, User.class);
+        return response.getBody();
+    }
+
+    /**
+     * 出现短路请求返回的缺省值
+     *
+     * @param id
+     * @return
+     */
+    public User getUserByIdFallBack(String id, Throwable throwable) {
+        log.info("进入getUserByIdFallBack(..)回退方法，异常：" + throwable);
+        User user = new User();
+        user.setId(10000L);
+        user.setName("default user");
+        return user;
+    }
+
+
+    /**
+     * 出现短路，不会退
+     * 出现 int a = 1/0--》ArithmeticException异常是不回退
+     *
+     * @param id
+     * @return
+     */
+    @HystrixCommand(fallbackMethod = "getUserByIdFallBack", ignoreExceptions = {ArithmeticException.class})
+    @GetMapping("/noback/user/{id}")
+    public User notFallBack(@PathVariable String id) {
+        int a = 1/0;
+        String url = "http://user-provider/user-provider/user/" + id;
+        ResponseEntity<User> response = restTemplate.getForEntity(url, User.class);
         return response.getBody();
     }
 
     @GetMapping("/log-user-instance")
-    public void logUserInstance(){
+    public void logUserInstance() {
         //测试的时候启动多个user-provider-metadata实例
         ServiceInstance serviceInstance = loadBalancerClient.choose("user-provider-metadata");
         log.info("查看是哪个节点访问");
-        log.info("{}:{}:{}",serviceInstance.getServiceId(),serviceInstance.getHost(),serviceInstance.getPort());
+        log.info("{}:{}:{}", serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
     }
 }
