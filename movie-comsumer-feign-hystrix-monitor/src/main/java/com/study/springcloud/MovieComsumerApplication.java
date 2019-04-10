87f3f1e0d2b78a1aef2f4fd9d1733743f6a09281
wangruigen <1028725142@qq.com>
2019-04-10 18:38:16 +0800

    hystrix可视化监控

--- movie-comsumer-feign-hystrix-monitor/src/main/java/com/study/springcloud/MovieComsumerApplication.java
+++ movie-comsumer-feign-hystrix-monitor/src/main/java/com/study/springcloud/MovieComsumerApplication.java
@@ -3,11 +3,10 @@ package com.study.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 @EnableHystrixDashboard //开启hystrix可视化监控
 public class MovieComsumerApplication {
 
