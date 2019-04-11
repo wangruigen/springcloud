f57b9e40756df3235ed52d211d9301f80fe2739b
wangruigen <1028725142@qq.com>
2019-04-11 13:45:57 +0800

    开启hystrixDashboard监控

--- movie-comsumer-feign-hystrix-fallback/src/main/java/com/study/springcloud/MovieComsumerApplication.java
+++ movie-comsumer-feign-hystrix-fallback/src/main/java/com/study/springcloud/MovieComsumerApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableHystrix
 @EnableFeignClients //开启feignClient
 public class MovieComsumerApplication {
 
