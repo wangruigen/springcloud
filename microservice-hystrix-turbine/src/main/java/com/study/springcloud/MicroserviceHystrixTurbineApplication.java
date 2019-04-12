164ad08d85cdcd1765c1ad09367751c60b9bc2b3
wangruigen <1028725142@qq.com>
2019-04-12 13:58:21 +0800

    turbine监控多个微服务

--- microservice-hystrix-turbine/src/main/java/com/study/springcloud/MicroserviceHystrixTurbineApplication.java
+++ microservice-hystrix-turbine/src/main/java/com/study/springcloud/MicroserviceHystrixTurbineApplication.java
@@ -2,8 +2,10 @@ package com.study.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
+@EnableTurbine //开启turbine
 public class MicroserviceHystrixTurbineApplication {
 
     public static void main(String[] args) {
