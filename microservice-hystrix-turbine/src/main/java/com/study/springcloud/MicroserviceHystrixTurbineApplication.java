b1a9620575a3bdd72c6bac60f5364d8371db4e7f
wangruigen <1028725142@qq.com>
2019-04-12 11:14:04 +0800

    使用turbine监控多个微服务

---
+++ microservice-hystrix-turbine/src/main/java/com/study/springcloud/MicroserviceHystrixTurbineApplication.java
@@ -0,0 +1,14 @@
+package com.study.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceHystrixTurbineApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MicroserviceHystrixTurbineApplication.class, args);
+    }
+
+}
