834df5e96700bfbce01fc9d09adbadad8665be2d
wangruigen <1028725142@qq.com>
2019-04-18 15:27:13 +0800

    config server

--- microservice-config-server/src/main/java/com/study/springcloud/ConfigServerApplication.java
+++ microservice-config-server/src/main/java/com/study/springcloud/ConfigServerApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer //声明是config server
+//声明是config server
+@EnableConfigServer
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
