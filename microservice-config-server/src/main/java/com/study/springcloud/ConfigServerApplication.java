d19f679c148c6edd381fa3c08b9b46eeaa9c3fe1
wangruigen <1028725142@qq.com>
2019-04-18 15:24:02 +0800

    delete config server

--- microservice-config-server/src/main/java/com/study/springcloud/ConfigServerApplication.java
+++ microservice-config-server/src/main/java/com/study/springcloud/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.study.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer //声明是config server
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
