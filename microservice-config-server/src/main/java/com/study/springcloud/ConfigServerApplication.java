68cc5c708daa700515d219e45683589c66930282
wangruigen <1028725142@qq.com>
2019-04-18 15:04:50 +0800

    spring cloud config-server

--- microservice-config-server/src/main/java/com/study/springcloud/ConfigServerApplication.java
+++ microservice-config-server/src/main/java/com/study/springcloud/ConfigServerApplication.java
@@ -2,14 +2,12 @@ package com.study.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaServiceDescoveryApplication {
+public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServiceDescoveryApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 
 }
