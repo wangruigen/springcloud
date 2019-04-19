45b369fcec833e350639c2043e9cc077a482d282
wangruigen <1028725142@qq.com>
2019-04-19 12:32:25 +0800

    Md

--- microservice-config-server/src/main/java/com/study/springcloud/ConfigServerApplication.java
+++ microservice-config-server/src/main/java/com/study/springcloud/ConfigServerApplication.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
+        //
         SpringApplication.run(ConfigServerApplication.class, args);
     }
 
