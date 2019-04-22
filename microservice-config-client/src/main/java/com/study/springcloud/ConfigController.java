d64f902e8b934019fc989bfebeec5f99fe6bef6a
wangruigen <1028725142@qq.com>
2019-04-22 09:09:53 +0800

    config-client /refresh端点刷新配置

--- microservice-config-client/src/main/java/com/study/springcloud/ConfigController.java
+++ microservice-config-client/src/main/java/com/study/springcloud/ConfigController.java
@@ -1,10 +1,16 @@
 package com.study.springcloud;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+/**
+ * @RefreshScope 该注解表示刷新配置时会做处理
+ * 手动刷新git仓库的值：通过post 访问config client的/refresh断点进行手动刷新git仓库的配置值。
+ */
+@RefreshScope
 public class ConfigController {
 
     /**
