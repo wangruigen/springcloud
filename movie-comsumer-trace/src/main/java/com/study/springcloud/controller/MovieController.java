9d22f412944468d1e5d3db31b72bccfba9d838d8
wangruigen <1028725142@qq.com>
2019-04-22 10:23:28 +0800

    电影微服务-消费者-整合sleuth

---
+++ movie-comsumer-trace/src/main/java/com/study/springcloud/controller/MovieController.java
@@ -0,0 +1,26 @@
+package com.study.springcloud.controller;
+
+import com.study.springcloud.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class MovieController {
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public MovieController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping("/user/{id}")
+    public User getUserById(@PathVariable String id){
+        ResponseEntity<User> response = restTemplate.getForEntity("http://localhost:8088/user/"+id, User.class);
+        return response.getBody();
+    }
+}
