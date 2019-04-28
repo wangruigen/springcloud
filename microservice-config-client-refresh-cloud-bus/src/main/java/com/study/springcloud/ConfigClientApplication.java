76e5008a0e8107358971830c8a0df465e254d63f
wangruigen <1028725142@qq.com>
2019-04-28 20:41:23 +0800

    springcloud bus

---
+++ microservice-config-client-refresh-cloud-bus/src/main/java/com/study/springcloud/ConfigClientApplication.java
@@ -0,0 +1,17 @@
+package com.study.springcloud;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+
+}
